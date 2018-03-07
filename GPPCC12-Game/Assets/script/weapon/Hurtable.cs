d2c1def87c46accd12035d631d0b54d1aa9cb87a
Sebastian Zumbrunn <sebastian.zumbrunn@gmail.com>
2018-03-07 22:43:39 +0100

    added project settings dir

---
+++ GPPCC12-Game/Assets/script/weapon/Hurtable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Hurtable : MonoBehaviour
+{
+	[SerializeField]
+	private int life;
+
+	[SerializeField]
+	private int maxLife;
+
+
+}
---
+++ GPPCC12-Game/Assets/script/weapon/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+	[SerializeField]
+	private int damage;
+
+	public abstract void Trigger();
+
+	public int Damage
+	{
+		get { return damage; }
+		set { damage = value; }
+	}
+}
