b2406eb7f7ccc10b683138abb2200ca38d3efac1
Sebastian Zumbrunn <sebastian.zumbrunn@gmail.com>
2018-03-12 21:59:46 +0100

    - shooting works now online - some minor adjustments

--- GPPCC12-Game/Assets/script/item/weapon/ProjectileWeapon.cs
+++ GPPCC12-Game/Assets/script/item/weapon/ProjectileWeapon.cs
@@ -5,25 +5,17 @@ using UnityEngine.Networking;
 
 public class ProjectileWeapon : Weapon
 {
-	public Projectile projectil;
+	public Spawner.SpawnerPrefab projectil;
     public Spawner spawner;
+	public Camera camera;
 
 	void Start()
 	{
 		TriggerEvent += OnTriggerEvent;
-		projectil.parentWeapon = this;
 	}
 
 	private void OnTriggerEvent(Weapon weapon)
 	{
-		CmdSpawnProjectile();
-	}
-
-	[Command]
-	private void CmdSpawnProjectile()
-	{
-		//GameObject go = Instantiate(projectil.gameObject, spawner.transform.position, spawner.transform.rotation);
-        Debug.Log("Projectile " + projectil.gameObject);
-        spawner.CmdSpawn(projectil.gameObject, spawner.transform.position, spawner.transform.rotation);
+		spawner.CmdSpawn((int) projectil, parent.transform.position, camera.transform.rotation);
 	}
 }
--- GPPCC12-Game/Assets/script/network/Spawner.cs
+++ GPPCC12-Game/Assets/script/network/Spawner.cs
@@ -5,11 +5,26 @@ using UnityEngine.Networking;
 
 public class Spawner : NetworkBehaviour
 {
+	public  GameObject[] prefabs;
+
 	[Command]
-	public void CmdSpawn(GameObject prefab, Vector3 pos, Quaternion rot)
+	public void CmdSpawn(int sp, Vector3 pos, Quaternion rot)
 	{
-        Debug.Log("Spawn " + prefab);
-        GameObject go = Instantiate(prefab, pos, rot);
-		NetworkServer.Spawn(go);
+		if (sp >= 0 && sp < prefabs.Length)
+		{
+			GameObject prefab = prefabs[sp];
+			GameObject go = Instantiate(prefab, pos, rot);
+			NetworkServer.Spawn(go);
+		}
+		else
+		{
+			Debug.LogError("SpawnerPrefab " + sp + " doesn't exist!");
+			return;
+		}
+	}
+
+	public enum SpawnerPrefab
+	{
+		LauncherBullet
 	}
 }
