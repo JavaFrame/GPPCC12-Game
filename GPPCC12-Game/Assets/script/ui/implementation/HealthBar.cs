ed7869150fd9f5c1adad56d495c98d6d14cecb3c
MCPGamer <Duarte.amarelo@hotmail.com>
2018-03-19 21:54:37 +0100

    Added HPBar Prefab (Requires to be placed in Canvas)

---
+++ GPPCC12-Game/Assets/script/ui/implementation/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : Hurtable {
+
+    [SerializeField]
+    private DeathHurtable hurtable;
+
+    [SerializeField]
+    private Text lbHP;
+
+    [SerializeField]
+    private Image hpBar;
+
+    private void Start()
+    {
+        lbHP.text = "HP: " + hurtable.life + " / " + hurtable.maxLife;
+        hurtable.HittedEventHandler += (damage, from, weapon) => hpBar.fillAmount = (float) hurtable.life / hurtable.maxLife;
+    }
+}
